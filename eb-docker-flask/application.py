f5060692a969ce95a6423f4ba6f5e0089574f576
Nicholas Tamburri <nicholas.tamburri1@marist.edu>
2018-12-18 00:48:27 +0000

    Add form to application

--- eb-docker-flask/application.py
+++ eb-docker-flask/application.py
@@ -1,10 +1,10 @@
 # This is based on the Single Container Docker Environments tutorial for
-# AWS Elastic Beanstalk, together with the IBM Cloud Language Tranlator API.
+# AWS Elastic Beanstalk, together with the IBM Cloud Language Translator API.
 #
 # https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/single-container-docker.html
 # https://cloud.ibm.com/apidocs/language-translator?language=python
 
-from flask import Flask
+from flask import Flask, request
 from watson_developer_cloud import LanguageTranslatorV3, WatsonApiException
 
 import json
@@ -28,6 +28,14 @@ model_names.sort()
 models_str = ''
 for model in model_names:
     models_str += model + '<br>'
+model_dropdown = '<select name="model" id="model">'
+for model in model_names:
+    if model == 'en-es':
+        model_dropdown += '<option value="{0}" selected>{0}</option>'.format(
+            model)
+    else:
+        model_dropdown += '<option value="{0}">{0}</option>'.format(model)
+model_dropdown += '</select>'
 
 
 # translate a word.
@@ -55,12 +63,20 @@ intro = '<p>Welcome to the word translator!</p>\n'
 instructions = '''
     <p><em>Hint</em>: This is a RESTful web service! Append a word and model
     to the URL (for example: <code>/hello/en-es</code>) to see the word in
-    another language.</p>\n
+    another language. Alternatively, you may use the form.</p>\n
 
     <p>A model is <code>&ltsource&gt-&lttarget&gt</code> where &ltsource&gt is
     the code of the language to translate from and &lttarget&gt is the code of
-    the language to translate to. Here are the available language codes
-    and models:</p>\n'''
+    the language to translate to. The available langauge codes and models are
+    listed below.</p>\n'''
+
+form = '''
+    <form action="/form-translate" method="post">
+    <label for="word">Word to translate:</label>
+    <input name="word" id="word" value="hello">
+    <label for="model">Model:</label> {}
+    <input type="submit" value="Translate">
+    </form>'''.format(model_dropdown)
 
 table = '''<table><tr><th>Language codes</th><th>Models</th></tr>
     <tr><td>{}</td><td>{}</td></tr></table>'''.format(
@@ -76,12 +92,22 @@ application = Flask(__name__)
 # add a rule for the index page.
 application.add_url_rule(
     '/', 'index', (lambda: header_text +
-    intro + instructions + table + footer_text))
+    intro + instructions + form + table + footer_text))
 
 # add a rule to translate a word.
 application.add_url_rule(
     '/<word>/<model>', 'translate', (lambda word, model:
     header_text + translate(word, model) + home_link + footer_text))
+
+# add a rule to translate a word using the form
+def form_translate():
+    if request.headers.get('Content-Type')\
+            == 'application/x-www-form-urlencoded':
+        return header_text + translate(request.form['word'],
+                                       request.form['model'])\
+            + home_link + footer_text
+application.add_url_rule(
+    '/form-translate', 'form_translate', form_translate, methods=['POST'])
 
 # run the app.
 if __name__ == "__main__":
